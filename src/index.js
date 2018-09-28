a336f18039f66c94aa3f6cf345267e31b074bbf7
zhekaprb <zheka_cool@mail.ru>
2018-09-29 01:08:29 +0300

    100 test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+ let zeros = 0;
+
+ for (let i = 5; i < 100000000; i*=5) {
+
+  let tempNumber = number;
+
+    while(tempNumber/i >= 1) {
+      zeros +=Math.floor(tempNumber/i);
+      tempNumber=tempNumber%i;
+    }
+ }
+
+ return zeros;
 }
